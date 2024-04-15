4273c607f5e4fa2f6f6b408e7fe259bfea414a3f
Andriy29k <adnriykorotchuk345@gmail.com>
2024-04-15 12:03:21 +0300

    Lab2

---
+++ WorkWithWindows/WorkWithWindows/Window1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WorkWithWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        int count;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!IsVisible)
+                return;
+            e.Cancel = true;
+            if (MessageBox.Show("Закрити підпорядковане вікно?", "Підтвердження", MessageBoxButton.YesNo,
+                MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.Yes)
+            {
+                Hide();
+                Owner.Activate();
+            }
+        }
+
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            (Owner.FindName("button1") as Button).Content = IsVisible ?
+                "Закрити підпорядковане вікно" : "Вікдрити підпорядковане віно";
+            if (IsVisible)
+                textBlock.Text = "Вікно відкрите" + (++count) + "-й раз.";
+        }
+    }
+}
---
+++ WorkWithWindows/WorkWithWindows/Window2.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WorkWithWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (IsVisible)
+                dialogRes = false;
+            textBox1.Focus();
+        }
+
+        private void button21_Click(object sender, RoutedEventArgs e)
+        {
+            button22_Click(null, null);
+            dialogRes = true;
+            Close();
+        }
+
+        public void button22_Click(object sender, RoutedEventArgs e)
+        {
+            Owner.Title = textBox1.Text;
+            Owner.OwnedWindows[0].Title = textBox2.Text;
+        }
+        bool dialogRes;
+        public bool DialogRes { get { return dialogRes; } }
+    }
+}
