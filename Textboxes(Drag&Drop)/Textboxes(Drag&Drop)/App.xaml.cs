887beaf26f8376b0d3584248c6f6751dc6081a64
Andriy29k <adnriykorotchuk345@gmail.com>
2024-04-19 21:22:15 +0300

    Lab7

---
+++ Textboxes(Drag&Drop)/Textboxes(Drag&Drop)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Textboxes_Drag_Drop_
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
