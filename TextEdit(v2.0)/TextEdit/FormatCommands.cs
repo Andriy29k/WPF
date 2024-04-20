e051bf495711f660fc13b0a81eed14c00cffab9e
Andriy29k <adnriykorotchuk345@gmail.com>
2024-04-20 12:39:18 +0300

    Lab10

---
+++ TextEdit(v2.0)/TextEdit/FormatCommands.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TextEdit
+{
+    class FormatCommands
+    {
+        private static RoutedUICommand fontcol;
+        private static RoutedUICommand backcol;
+        static FormatCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.F, ModifierKeys.Control | ModifierKeys.Shift, "Ctrl+Shift+F"));
+            fontcol = new RoutedUICommand("Font Color...", "FontColor", typeof(FormatCommands), inputs);
+            inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.B, ModifierKeys.Control | ModifierKeys.Shift, "Ctrl+Shift+B"));
+            backcol = new RoutedUICommand("Back Color...", "BackColor", typeof(FormatCommands), inputs);
+        }
+
+        public static RoutedUICommand FontColor { get { return fontcol; } }
+
+        public static RoutedUICommand BackColor { get { return backcol; } }
+    }
+}
