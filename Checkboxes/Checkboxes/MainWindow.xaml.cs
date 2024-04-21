ecf21e21dff2a4d8c0d518d84ce81e7296b63da1
Andriy29k <adnriykorotchuk345@gmail.com>
2024-04-21 09:18:22 +0300

    Lab16

---
+++ Checkboxes/Checkboxes/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Checkboxes
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Window1 win1 = new Window1();
+        public MainWindow()
+        {
+            InitializeComponent();
+            win1.listBox1.ItemsSource = MakeCheckBoxList(5);
+            win1.listBox1.ItemsSource = MakeCheckBoxList(4);
+            win1.listBox1.ItemsSource = MakeCheckBoxList(6);
+            for(int i = 0; i < 3; i++)
+                (win1.grid1.Children[i] as ListBox).SelectedIndex = 0;
+            button1.Focus();
+        }
+
+        IEnumerable<CheckBox> MakeCheckBoxList(int count)
+        {
+            return Enumerable.Range(1, count).Select(e =>
+            {
+                var res = new CheckBox();
+                res.Content = e.ToString();
+                res.IsTabStop = false;
+                return res;
+            });
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            win1.ShowDialog();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkBox1_Click(object sender, RoutedEventArgs e)
+        {
+            var cb = e.Source as CheckBox;
+            if (cb != null) return;
+            var i = Grid.GetRow(cb);
+            var tb = grid1.Children[i] as TextBlock;
+            tb.Text = cb.IsChecked == true ? "Вибрані всі пункти" : "Пункти не вибрані";
+            var lb = win1.grid1.Children[i] as ListBox;
+            foreach ( var e1 in lb.Items.Cast<CheckBox>())
+                e1.IsChecked=cb.IsChecked;
+        }
+    }
+}
---
+++ Checkboxes/Checkboxes/Window1.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Checkboxes
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            (listBox1.SelectedItem as CheckBox).Focus();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide(); 
+            for(int i=0;i<3;i++)
+            {
+                ListBox lb = grid1.Children[i] as ListBox;
+                var a = lb.Items.Cast<CheckBox>().Where(e1 => (bool)e1.IsChecked);
+                var tb = (Owner as MainWindow).grid1.Children[i] as TextBox;
+                var cb = (Owner as MainWindow).grid1.Children[i] as CheckBox;
+                if (a.Count() == lb.Items.Count)
+                {
+                    tb.Text = "Вибрані всі пункти";
+                    cb.IsChecked = true;
+                }
+                else if (a.Count() == 0)
+                {
+                    tb.Text = "Пункти не вибрані";
+                    cb.IsChecked = false;
+                }
+                else
+                {
+                    tb.Text = a.Aggregate("Вибрано:", (acc,e1)=>acc+""+e1.Content);
+                    cb.IsChecked = null;
+                }
+
+            }
+        }
+
+        private void listBox1_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                var lb = e.Source as ListBox;
+                if (lb == null)
+                    return;
+                var cb=lb.SelectedItems as CheckBox;
+                cb.IsChecked = !(bool)cb.IsChecked;
+            }
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            (listBox1.SelectedItem as CheckBox).Focus();
+        }
+    }
+}
