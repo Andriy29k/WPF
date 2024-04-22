3058b432e5c16432be6ed6cdd1eafd656cb129f8
Andriy29k <adnriykorotchuk345@gmail.com>
2024-04-22 10:58:12 +0300

    Individual task var 6

---
+++ IndividualTask1/IndividualTask1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace IndividualTask1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Window1 win1 =  new Window1();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AddLabel_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Window1();
+            dialog.Owner = this; 
+            if (dialog.ShowDialog() == true)
+            {
+                double x, y;
+                if (double.TryParse(dialog.txtX.Text, out x) && double.TryParse(dialog.txtY.Text, out y))
+                {
+                    var label = new System.Windows.Controls.Label();
+                    label.Content = dialog.txtText.Text;
+                    Canvas.SetLeft(label, x);
+                    Canvas.SetTop(label, y);
+                    canvas1.Children.Add(label);
+                }
+            }
+        }
+    }
+}
---
+++ IndividualTask1/IndividualTask1/Window1.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IndividualTask1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private double previousX = 0;
+        private double previousY = 0;
+        private bool usePreviousCoordinates = false;
+        public Window1()
+        {
+            InitializeComponent();
+            this.KeyDown += AddLabelDialog_KeyDown;
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+
+        }
+
+        private void AddLabelDialog_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (Ok.IsDefault)
+                    Apply_Click(sender, e);
+                else if (Apply.IsDefault)
+                    Apply_Click(sender, e);
+            }
+            else if (e.Key == Key.Escape)
+            {
+                if (Cancel.IsCancel)
+                    Cancel_Click(sender, e);
+            }
+        }
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            if (usePreviousCoordinates)
+            {
+
+                txtX.Text = (previousX + 20).ToString();
+                txtY.Text = (previousY + 20).ToString();
+            }
+            else
+            {
+
+                txtX.Text = "";
+                txtY.Text = "";
+            }
+            txtX.Focus();
+        }
+
+        private void SetPreviousCoordinates(double x, double y)
+        {
+            previousX = x;
+            previousY = y;
+            usePreviousCoordinates = true;
+        }
+    }
+}
